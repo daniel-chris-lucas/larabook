a53c9e82bdf12f09c498d9022177b54c9235c376
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-04-21 10:45:09 +0200

    Added registration with BDD

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,19 @@
 <?php
 
-Route::get('/', 'PagesController@home');
+Route::get('/', [
+    'as'   => 'home',
+    'uses' => 'PagesController@home'
+]);
+
+/**
+ * Registration!
+ */
+Route::get('register', [
+    'as'   => 'register_path',
+    'uses' => 'RegistrationController@create'
+]);
+
+Route::post('register', [
+    'as'   => 'register_path',
+    'uses' => 'RegistrationController@store'
+]);
